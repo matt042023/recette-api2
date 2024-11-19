2fad4b1b9811d5a028f26928e7e2dace9a45f953
matt042023 <matt.lheureux@hotmail.fr>
2024-11-19 23:18:32 +0100

    1 - creation de la class Recipe

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 128)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 128)]
+    private ?string $slug = null;
+
+    #[ORM\Column]
+    private ?bool $draft = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $cooking = null;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $break = null;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $preparation = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function isDraft(): ?bool
+    {
+        return $this->draft;
+    }
+
+    public function setDraft(bool $draft): static
+    {
+        $this->draft = $draft;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCooking(): ?int
+    {
+        return $this->cooking;
+    }
+
+    public function setCooking(?int $cooking): static
+    {
+        $this->cooking = $cooking;
+
+        return $this;
+    }
+
+    public function getBreak(): ?int
+    {
+        return $this->break;
+    }
+
+    public function setBreak(?int $break): static
+    {
+        $this->break = $break;
+
+        return $this;
+    }
+
+    public function getPreparation(): ?int
+    {
+        return $this->preparation;
+    }
+
+    public function setPreparation(?int $preparation): static
+    {
+        $this->preparation = $preparation;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
