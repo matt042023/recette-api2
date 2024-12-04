8de1342e4b11493fec14a59173ba6b4b3cac9b65
matt042023 <matt.lheureux@hotmail.fr>
2024-12-04 16:10:45 +0100

    modification descriptionTrait

--- src/Entity/Traits/HasDescriptionTrait.php
+++ src/Entity/Traits/HasDescriptionTrait.php
@@ -5,11 +5,16 @@ namespace App\Entity\Traits;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 trait HasDescriptionTrait
 {
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     #[Groups(['Recipe:item:get'])]
+    #[Assert\Length(
+        max: 5000, 
+        maxMessage: "La description ne peut pas dépasser {{ limit }} caractères"
+    )]
     private ?string $description = null;
 
     public function getDescription(): ?string
@@ -22,5 +27,13 @@ trait HasDescriptionTrait
         $this->description = $description;
 
         return $this;
+    }
+
+    // Ajout d'une méthode pour nettoyer le texte
+    public function sanitizeDescription(): void
+    {
+        if ($this->description) {
+            $this->description = trim(strip_tags($this->description));
+        }
     }
 }
