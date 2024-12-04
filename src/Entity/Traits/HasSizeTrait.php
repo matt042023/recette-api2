b3b41db2c08b33288befdfa03e6ec7dff67ac8dc
matt042023 <matt.lheureux@hotmail.fr>
2024-12-04 16:15:19 +0100

    modification sizeTrait

--- src/Entity/Traits/HasSizeTrait.php
+++ src/Entity/Traits/HasSizeTrait.php
@@ -4,12 +4,19 @@ namespace App\Entity\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 trait HasSizeTrait
 {
     #[ORM\Column]
     #[Groups(['Recipe:item:get'])]
-    private int $size;
+    #[Assert\Positive(message: "La taille doit être un nombre positif")]
+    #[Assert\Range(
+        min: 1,
+        max: 1000000,
+        notInRangeMessage: "La taille doit être entre {{ min }} et {{ max }}"
+    )]
+    private int $size = 1; // Valeur par défaut
 
     public function getSize(): int
     {
@@ -18,7 +25,7 @@ trait HasSizeTrait
 
     public function setSize(int $size): static
     {
-        $this->size = $size;
+        $this->size = max(1, $size);
 
         return $this;
     }
