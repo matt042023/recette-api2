1d3f9e4ce2f0733f38c15f6f6bdb7b8fe49f0a11
matt042023 <matt.lheureux@hotmail.fr>
2024-12-04 16:07:47 +0100

    modification nameTrait

--- src/Entity/Traits/HasNameTrait.php
+++ src/Entity/Traits/HasNameTrait.php
@@ -5,15 +5,27 @@ namespace App\Entity\Traits;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 trait HasNameTrait
 {
     #[ORM\Column(length: 128)]
     #[Groups(['Recipe:item:get'])]
-    private string $name;
+    #[Assert\NotBlank(message: "Le nom ne peut pas être vide")]
+    #[Assert\Length(
+        min: 2, 
+        max: 128, 
+        minMessage: "Le nom doit faire au moins {{ limit }} caractères",
+        maxMessage: "Le nom ne peut pas dépasser {{ limit }} caractères"
+    )]
+    private string $name = '';
 
     #[ORM\Column(length: 128, unique: true)]
     #[Gedmo\Slug(fields: ['name'], unique: true)]
+    #[Assert\Regex(
+        pattern: '/^[a-z0-9-]+$/', 
+        message: "Le slug ne peut contenir que des lettres minuscules, des chiffres et des traits d'union"
+    )]
     private string $slug;
 
     public function getName(): string
@@ -39,4 +51,14 @@ trait HasNameTrait
 
         return $this;
     }
+
+     // Ajout d'une méthode pour générer manuellement un slug si nécessaire
+     public function generateSlug(): string
+     {
+         $slug = transliterator_transliterate(
+             'Any-Latin; Latin-ASCII; Lower(); [:Punctuation:] Remove; Space_Remove',
+             $this->name
+         );
+         return str_replace(' ', '-', $slug);
+     }
 }
